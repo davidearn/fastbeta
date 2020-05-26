85b232cbad0b76dd408f9871719e4e193458916d
Mikael Jagan <jaganmn@mcmaster.ca>
2020-05-26 02:28:43 -0400

    Test mathjaxr

--- R/fastbeta.R
+++ R/fastbeta.R
@@ -1,29 +1,27 @@
+#' \loadmathjax
 #' The \pkg{fastbeta} package
 #'
 #' @details
 #' \pkg{fastbeta} implements the FC, S, and SI methods for estimating
-#' time-varying transmission rates
-#' \ifelse{latex}{\out{$\beta(t)$}}{\ifelse{html}{\out{<i>&beta;</i>(<i>t</i>)}}{beta(t)}}
-#' from time series data:
+#' time-varying transmission rates \mjeqn{\beta(t)}{beta(t)} from time
+#' series data:
 #'
 #' * [estimate_beta_FC()]
 #' * [estimate_beta_S()]
 #' * [estimate_beta_SI()]
 #'
-#' The SI method is substantially more accurate and robust than the
-#' FC and S methods, and should be preferred in practice.
+#' The SI method is substantially more robust than the FC and S methods
+#' and should be preferred in practice.
 #'
 #' \pkg{fastbeta} additionally implements peak-to-peak iteration (PTPI),
 #' a method for estimating the initial number of susceptibles
-#' \ifelse{latex}{\out{$S_0$}}{\ifelse{html}{\out{<i>S</i><sub>0</sub>}}{S_0}}
-#' from time series data:
+#' \mjeqn{S_0}{S_0} from time series data:
 #'
 #' * [ptpi()]
 #' * [get_peak_times()]
 #'
 #' PTPI can be used in conjunction with the FC, S, and SI methods,
-#' which require users to specify an estimate of
-#' \ifelse{latex}{\out{$S_0$}}{\ifelse{html}{\out{<i>S</i><sub>0</sub>}}{S_0}}.
+#' which require users to specify an estimate of \mjeqn{S_0}{S_0}.
 #'
 #' \pkg{fastbeta} includes functions useful for simulating incidence
 #' time series with an underlying, seasonally forced transmission rate.
@@ -36,27 +34,7 @@
 #' All methods are based on the SIR model with time-varying rates of
 #' birth, death, and transmission:
 #'
-#' \ifelse{latex}{
-#'   \out{
-#'     \begin{array}[rlc]
-#'       S' & = & \nu(t) \widehat{N}_0 - \beta(t) S I - \mu(t) S \\
-#'       I' & = & \beta(t) S I - \gamma I - \mu(t) I \\
-#'       R' & = & \gamma I - \mu(t) R \\
-#'     \end{array}
-#'   }
-#' }{
-#'   \ifelse{html}{
-#'     \out{
-#'       <i>S</i>&prime; = <i>&nu;</i>(<i>t</i>)<i>&Ntilde;</i><sub>0</sub> &minus; <i>&beta;</i>(<i>t</i>)<i>SI</i> &minus; <i>&mu;</i>(<i>t</i>)<i>S</i><br>
-#'       <i>I</i>&prime; = <i>&beta;</i>(<i>t</i>)<i>SI</i> &minus; <i>&gamma;I</i> &minus; <i>&mu;</i>(<i>t</i>)<i>I</i><br>
-#'       <i>R</i>&prime; = <i>&gamma;I</i> &minus; <i>&mu;</i>(<i>t</i>)<i>R</i>
-#'     }
-#'   }{
-#'     % S' = nu(t)*hatN_0 - beta(t)*S*I - mu(t)*S \cr
-#'     % I' = beta(t)*S*I - gamma*I - mu(t)*I \cr
-#'     % R' = gamma*I - mu(t)*R
-#'   }
-#' }
+#' \mjdeqn{\begin{align} \frac{\text{d}S}{\text{d}t} &= \nu(t) - \beta(t) S I - \mu(t) S \cr \frac{\text{d}I}{\text{d}t} &= \beta(t) S I - \gamma I - \mu(t) I \cr \frac{\text{d}R}{\text{d}t} &= \gamma I - \mu(t) \end{align}}{(1) dS/dt = nu(t) - beta(t) S I - mu(t) S    (2) dI/dt = beta(t) S I - gamma I - mu(t) I    (3) dR/dt = gamma I - mu(t) R}
 #'
 #' They are fully described in the referenced manuscript.
 #'
