32e9c54e09c0f89f2f0131821167cc8fd7665fe8
Mikael Jagan <jaganmn@mcmaster.ca>
2020-06-04 22:09:49 -0400

    Create impute functions

---
+++ R/impute_na.R
@@ -0,0 +1,17 @@
+impute_na <- function(x, y) {
+
+# Do nothing if there are no `NA` in `y`
+is_na <- is.na(y)
+if (!any(is_na)) {
+  return(y)
+}
+
+# `y` with `NA` imputed by linear interpolation (where possible)
+stats::approx(
+  x      = x[!is_na],
+  y      = y[!is_na],
+  xout   = x,
+  method = "linear",
+  rule   = 1 # return `NA` outside the range of `x[!is_na]`
+)
+}
