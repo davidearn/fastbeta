6c8fbfe5832b95dccfbd50e5a00e79d4ec069966
Mikael Jagan <jaganmn@mcmaster.ca>
2023-06-12 15:31:14 -0400

    API tweaks

--- R/fastbeta.R
+++ R/fastbeta.R
@@ -1,53 +1,54 @@
 fastbeta <-
-function(cases, births, mu, gamma, S0, I0) {
+function(Z, B, mu, gamma, S0, I0) {
     stopifnot(exprs = {
-        is.numeric(cases)
-        min(0, cases, na.rm = TRUE) >= 0
-        is.numeric(births)
-        length(births) == length(cases)
-        min(0, births, na.rm = TRUE) >= 0
+        is.numeric(Z)
+        min(0, Z, na.rm = TRUE) >= 0
+        is.numeric(B)
+        length(B) == length(Z)
+        min(0, B, na.rm = TRUE) >= 0
         is.double(mu)
-        length(mu) == length(cases)
+        length(mu) == length(Z)
         min(0, mu, na.rm = TRUE) >= 0
         is.double(gamma)
         length(gamma) == 1L
         gamma >= 0
-        is.double(S0)
+        is.numeric(S0)
         length(S0) == 1L
         S0 >= 0
-        is.double(I0)
+        is.numeric(I0)
         length(I0) == 1L
         I0 >= 0
     })
-    storage.mode(cases) <- storage.mode(births) <- "double"
-    .Call(R_fastbeta, cases, births, mu, gamma, S0, I0)
+    storage.mode(Z) <- storage.mode(B) <-
+        storage.mode(S0) <- storage.mode(I0) <- "double"
+    as.data.frame(.Call(R_fastbeta, Z, B, mu, gamma, S0, I0))
 }
 
 ptpi <-
-function(cases, births, mu,
+function(Z, B, mu,
          start, a = 1L, b = length(cases),
          tol = 1e-06, iter.max = 20L, complete = FALSE) {
     stopifnot(exprs = {
-        is.numeric(cases)
-        length(cases) >= 2L
-        min(0, cases, na.rm = TRUE) >= 0
-        is.numeric(births)
-        length(births) == length(cases)
-        min(0, births, na.rm = TRUE) >= 0
+        is.numeric(Z)
+        length(Z) >= 2L
+        min(0, Z, na.rm = TRUE) >= 0
+        is.numeric(B)
+        length(B) == length(Z)
+        min(0, B, na.rm = TRUE) >= 0
         is.double(mu)
-        length(mu) == length(cases)
+        length(mu) == length(Z)
         min(0, mu, na.rm = TRUE) >= 0
-        is.double(start)
+        is.numeric(start)
         length(start) == 1L
         start >= 0
         is.integer(a)
         length(a) == 1L
-        a >= 1L
-        a < length(cases)
+        a >= 0L
+        a < length(Z)
         is.integer(b)
         length(b) == 1L
         b > a
-        b <= length(cases)
+        b < length(Z)
         is.double(tol)
         length(tol) == 1L
         !is.na(tol)
@@ -58,6 +59,6 @@ function(cases, births, mu,
         length(complete) == 1L
         !is.na(complete)
     })
-    storage.mode(cases) <- storage.mode(births) <- "double"
-    .Call(R_ptpi, cases, births, mu, start, a, b, tol, iter.max, complete)
+    storage.mode(Z) <- storage.mode(B) <- storage.mode(start) <- "double"
+    .Call(R_ptpi, Z, B, mu, start, a, b, tol, iter.max, complete)
 }
