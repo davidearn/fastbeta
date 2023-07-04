76da905fe41c7afdc31ca20ac690e2b6ca617ce7
Mikael Jagan <jaganmn@mcmaster.ca>
2023-07-04 10:56:07 -0400

    waffling; also not forgetting to divide by 'prob'

--- R/deconvolve.R
+++ R/deconvolve.R
@@ -1,59 +1,64 @@
 deconvolve <-
-function(x, start, prob = 1, delay = 1,
-         tol = 1, iter.max = 20L, complete = FALSE)
+function(x, prob = 1, delay = 1,
+         start, tol = 1, iter.max = 20L, complete = FALSE)
 {
 	stopifnot(exprs = {
 		is.double(x)
 		length(x) >= 1L
-		is.double(prob)
-		any(length(prob) == c(1L, length(x)))
-		min(prob) >= 0
-		max(prob) <= 1
+		!anyNA(x)
 		is.double(delay)
 		length(delay) >= 1L
 		min(delay) >= 0
 		sum(delay) >  0
+		is.double(prob)
+		any(length(prob) == c(1L, length(x) + length(delay) - 1L))
+		min(prob) >= 0
+		max(prob) <= 1
+		is.double(start)
+		length(start) == length(x) + length(delay) - 1L
+		!anyNA(start)
 	})
 
 	n <- length(x)
 	d <- length(delay) - 1L
 	u <- rep.int(c(0, 1, 0), c(d, n, d))
-	i2  <- (d + 1L):(d + n    )
+	i2  <- (d + 1L):(d + n)
 	i23 <- (d + 1L):(d + n + d)
 
-	delay <- delay / sum(delay)
+	if ((delay.sum <- sum(delay)) != 1)
+		delay <- delay / delay.sum
 	delay.rev <- delay[(d + 1L):1L]
 	q <- filter(u, delay.rev, sides = 1)[i23]
 
-	lambda <- start
+	dx <- start
 	iter <- 0L
 	stol <- tol * n + 2 * sum(x)
 
 	if (complete) {
 		repeat {
-			y <- filter(lambda, delay, sides = 1)[i2]
-			s <- sum(y + x / (u[i2] <- y / x))
+			y <- filter(dx, delay, sides = 1)[i2]
+			s <- sum(y + x * (u[i2] <- x / y))
 			if (iter >= iter.max || s < stol)
 				break;
-			lambda <- lambda * filter(u, delay.rev, sides = 1)[i23] / q
+			dx <- dx * filter(u, delay.rev, sides = 1)[i23] / q
 			iter <- iter + 1L
 		}
-		list(lambda = lambda,
+		list(dx = if (missing(prob)) dx else dx / prob,
 		     chisq = (s - 2 * sum(x)) / n,
 		     iter = iter)
 	} else {
-		lambda. <- matrix(0, length(start), iter.max + 1L)
+		dx. <- matrix(0, length(start), iter.max + 1L)
 		s. <- double(iter.max + 1L)
 		repeat {
-			y <- filter(lambda, delay, sides = 1)[i2]
-			s.[iter + 1L] <- s <- sum(y + x / (u[i2] <- y / x))
-			lambda.[, iter + 1L] <- lambda
+			y <- filter(dx, delay, sides = 1)[i2]
+			s.[iter + 1L] <- s <- sum(y + x * (u[i2] <- x / y))
+			dx.[, iter + 1L] <- dx
 			if (iter >= iter.max || s < stol)
 				break;
-			lambda <- lambda * filter(u, delay.rev, sides = 1)[i23] / q
+			dx <- dx * filter(u, delay.rev, sides = 1)[i23] / q
 			iter <- iter + 1L
 		}
-		list(lambda = lambda.,
+		list(dx = if (missing(prob)) dx. else dx. / prob,
 		     chisq = (s. - 2 * sum(x)) / n,
 		     iter = iter)
 	}
