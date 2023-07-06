8e16d82cce240e3d41486be0e97210782a8b6898
Mikael Jagan <jaganmn@mcmaster.ca>
2023-07-05 22:46:47 -0400

    more robust deconvolution (incomplete)

--- R/deconvolve.R
+++ R/deconvolve.R
@@ -1,9 +1,12 @@
+## FIXME: not clearly robust to many zeros in 'x' and/or 'delay'
+
 deconvolve <-
 function(x, prob = 1, delay = 1,
-         start, tol = 1, iter.max = 20L, complete = FALSE)
+         start = 0.125 + c(double(length(delay) - 1L), x),
+         tol = 1, iter.max = 20L, complete = FALSE)
 {
 	stopifnot(exprs = {
-		is.double(x)
+		is.numeric(x)
 		length(x) >= 1L
 		!anyNA(x)
 		is.double(delay)
@@ -14,11 +17,12 @@ function(x, prob = 1, delay = 1,
 		any(length(prob) == c(1L, length(x) + length(delay) - 1L))
 		min(prob) >= 0
 		max(prob) <= 1
-		is.double(start)
+		is.numeric(start)
 		length(start) == length(x) + length(delay) - 1L
 		!anyNA(start)
 	})
 
+	storage.mode(x) <- storage.mode(start) <- "double"
 	n <- length(x)
 	d <- length(delay) - 1L
 	u <- rep.int(c(0, 1, 0), c(d, n, d))
@@ -31,22 +35,12 @@ function(x, prob = 1, delay = 1,
 	q <- filter(u, delay.rev, sides = 1)[i23]
 
 	dx <- start
+	if (min(q) <= 0)
+		dx[q <= 0] <- NaN
 	iter <- 0L
 	stol <- tol * n + 2 * sum(x)
 
 	if (complete) {
-		repeat {
-			y <- filter(dx, delay, sides = 1)[i2]
-			s <- sum(y + x * (u[i2] <- x / y))
-			if (iter >= iter.max || s < stol)
-				break;
-			dx <- dx * filter(u, delay.rev, sides = 1)[i23] / q
-			iter <- iter + 1L
-		}
-		list(dx = if (missing(prob)) dx else dx / prob,
-		     chisq = (s - 2 * sum(x)) / n,
-		     iter = iter)
-	} else {
 		dx. <- matrix(0, length(start), iter.max + 1L)
 		s. <- double(iter.max + 1L)
 		repeat {
@@ -60,6 +54,18 @@ function(x, prob = 1, delay = 1,
 		}
 		list(dx = if (missing(prob)) dx. else dx. / prob,
 		     chisq = (s. - 2 * sum(x)) / n,
+		     iter = iter)
+	} else {
+		repeat {
+			y <- filter(dx, delay, sides = 1)[i2]
+			s <- sum(y + x * (u[i2] <- x / y))
+			if (iter >= iter.max || s < stol)
+				break;
+			dx <- dx * filter(u, delay.rev, sides = 1)[i23] / q
+			iter <- iter + 1L
+		}
+		list(dx = if (missing(prob)) dx else dx / prob,
+		     chisq = (s - 2 * sum(x)) / n,
 		     iter = iter)
 	}
 }
