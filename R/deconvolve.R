2d10d7d8c84bfd549ecfa1c27bd5ded528dfc249
Mikael Jagan <jaganmn@mcmaster.ca>
2023-07-07 23:11:15 -0400

    more robust w.r.t. zeros

--- R/deconvolve.R
+++ R/deconvolve.R
@@ -1,14 +1,13 @@
-## FIXME: not clearly robust to many zeros in 'x' and/or 'delay'
-
 deconvolve <-
 function(x, prob = 1, delay = 1,
-         start = 0.125 + c(double(length(delay) - 1L), x),
+         start = c(rep.int(1, length(delay) - 1L), x),
          tol = 1, iter.max = 20L, complete = FALSE)
 {
 	stopifnot(exprs = {
 		is.numeric(x)
 		length(x) >= 1L
-		!anyNA(x)
+		min(x) >= 0
+		sum(x) >  0
 		is.double(delay)
 		length(delay) >= 1L
 		min(delay) >= 0
@@ -19,51 +18,84 @@ function(x, prob = 1, delay = 1,
 		max(prob) <= 1
 		is.numeric(start)
 		length(start) == length(x) + length(delay) - 1L
-		!anyNA(start)
+		min(start) >= 0
+		sum(start) >  0
 	})
 
 	storage.mode(x) <- storage.mode(start) <- "double"
-	n <- length(x)
+
+	n <- n. <- length(x)
 	d <- length(delay) - 1L
-	u <- rep.int(c(0, 1, 0), c(d, n, d))
-	i2  <- (d + 1L):(d + n)
-	i23 <- (d + 1L):(d + n + d)
+
+	if (min(x) == 0) {
+		i <- range(which(x > 0))
+		x <- x[i[1L]:i[2L]]
+		i <- i + c(0L, d)
+		start <- start[i[1L]:i[2L]]
+		n <- length(x)
+	}
+
+	if (min(prob) == 0)
+		prob[prob == 0] <- NaN
 
 	if ((delay.sum <- sum(delay)) != 1)
 		delay <- delay / delay.sum
 	delay.rev <- delay[(d + 1L):1L]
-	q <- filter(u, delay.rev, sides = 1)[i23]
 
-	dx <- start
+	u <- rep.int(c(0, 1, 0), c(d, n, d))
+	i2  <- (d + 1L):(d + n)
+	i23 <- (d + 1L):(d + n + d)
+
+	q <- filter(u, delay.rev, sides = 1)[i23]
+	q0 <- if (min(q) == 0) which(q == 0) else integer(0L)
+
+	r <- start
 	iter <- 0L
 	stol <- tol * n + 2 * sum(x)
 
 	if (complete) {
-		dx. <- matrix(0, length(start), iter.max + 1L)
+		r. <- matrix(0, d + n, iter.max + 1L)
 		s. <- double(iter.max + 1L)
 		repeat {
-			y <- filter(dx, delay, sides = 1)[i2]
+			if (length(q0))
+				r[q0] <- 0
+			y <- filter(r, delay, sides = 1)[i2]
+			r.[, iter + 1L] <- r
 			s.[iter + 1L] <- s <- sum(y + x * (u[i2] <- x / y))
-			dx.[, iter + 1L] <- dx
 			if (iter >= iter.max || s < stol)
-				break;
-			dx <- dx * filter(u, delay.rev, sides = 1)[i23] / q
+				break
+			r <- r * filter(u, delay.rev, sides = 1)[i23] / q
 			iter <- iter + 1L
 		}
-		list(dx = if (missing(prob)) dx. else dx. / prob,
-		     chisq = (s. - 2 * sum(x)) / n,
-		     iter = iter)
+		if (iter < iter.max) {
+			head <- seq_len(iter + 1L)
+			r. <- r.[, head, drop = FALSE]
+			s. <- s.[head]
+		}
+		if (length(q0))
+			r.[q0, ] <- NaN
+		if (n < n.)
+			r. <- `[<-`(matrix(NaN, d + n., iter + 1L), i[1L]:i[2L], , r.)
+		if (!missing(prob))
+			r. <- r. / prob
+		list(dx = r., chisq = (s. - 2 * sum(x)) / n, iter = iter)
 	} else {
 		repeat {
-			y <- filter(dx, delay, sides = 1)[i2]
+			if (length(q0))
+				r[q0] <- 0
+			y <- filter(r, delay, sides = 1)[i2]
 			s <- sum(y + x * (u[i2] <- x / y))
 			if (iter >= iter.max || s < stol)
-				break;
-			dx <- dx * filter(u, delay.rev, sides = 1)[i23] / q
+				break
+			r <- r * filter(u, delay.rev, sides = 1)[i23] / q
 			iter <- iter + 1L
 		}
-		list(dx = if (missing(prob)) dx else dx / prob,
-		     chisq = (s - 2 * sum(x)) / n,
-		     iter = iter)
+		if (length(q0))
+			r[q0] <- NaN
+		if (n < n.)
+			r <- `[<-`(rep.int(NaN, d + n.), i[1L]:i[2L], r)
+		if (!missing(prob))
+			r <- r / prob
+		list(dx = r, chisq = (s - 2 * sum(x)) / n, iter = iter)
 	}
 }
