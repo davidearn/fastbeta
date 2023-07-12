6157d2ac381dbbb6f18d8535803db9939bdbffe9
Mikael Jagan <jaganmn@mcmaster.ca>
2023-07-12 12:33:10 -0400

    optionally deconvolve in 'bootstrap'

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -13,15 +13,15 @@ function (r, series, constants, ...)
 	})
 
 	nu. <-
-	    if(TRUE)
-	        suppressWarnings(
-	            c(1, -1) * cumsum(c(1, rep_len(c(-2, 2), n)) * series[, 2L]))
-	    else {
-	        L <- suppressWarnings(
-	            matrix(c(0.5, 0.5, double(n)), n + 1L, n + 1L))
-	        L[1L] <- 1
-	        forwardsolve(L, series[, 2L])
-	    }
+		if (TRUE)
+			suppressWarnings(
+				c(1, -1) * cumsum(c(1, rep_len(c(-2, 2), n)) * series[, 2L]))
+		else {
+			L <- suppressWarnings(
+				matrix(c(0.5, 0.5, double(n)), n + 1L, n + 1L))
+			L[1L] <- 1
+			forwardsolve(L, series[, 2L])
+		}
 
 	n <- nrow(series) - 1L
 	s <- as.double(0:n)
@@ -33,10 +33,18 @@ function (r, series, constants, ...)
 	                  method = "linear", rule = 2:1, ties = "ordered")
 
 	constants. <- c(constants, 0) # R0 is irrelevant
+
+	fastbeta. <- function (series, constants, ..., prob, delay, tol, iter.max) {
+		call <- match.call(expand.dots = FALSE)
+		call[[1L]] <- "fastbeta"
+		call[["..."]] <- NULL
+		eval(call, parent.frame())
+	}
+
 	R <- replicate(r, {
 		X <- sir(n, beta, nu, mu, constants., ...)
 		series[, 1L:2L] <<- X[, c(4L, ncol(X))]
-		fastbeta(series, constants)[, 1L]
+		fastbeta.(series, constants, ...)[, 1L]
 	})
 	oldClass(R) <- oldClass(series)
 	tsp(R) <- tsp(series)
