b4fa4c70b673b3fa9970e51e5c21e080d3c28feb
Mikael Jagan <jaganmn@mcmaster.ca>
2024-04-29 11:57:45 -0400

    C interface for 'seir'

--- src/init.c
+++ src/init.c
@@ -5,29 +5,29 @@
 SEXP R_fastbeta(SEXP, SEXP);
 SEXP R_ptpi(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
-SEXP R_adsir_initialize(SEXP, SEXP, SEXP, SEXP, SEXP);
-SEXP R_adsir_finalize(void);
-SEXP R_adsir_dot(SEXP, SEXP);
-SEXP R_adsir_jac(SEXP, SEXP);
+SEXP R_adseir_initialize(SEXP, SEXP, SEXP, SEXP, SEXP);
+SEXP R_adseir_finalize(void);
+SEXP R_adseir_dot(SEXP, SEXP);
+SEXP R_adseir_jac(SEXP, SEXP);
 
-SEXP R_desir_initialize(SEXP, SEXP, SEXP, SEXP, SEXP);
-SEXP R_desir_finalize(void);
-void R_desir_dot(int *, double *, double *, double *, double *, int *);
-void R_desir_jac(int *, double *, double *, int *, int *, double *, int *,
-                 double *, int *);
+SEXP R_deseir_initialize(SEXP, SEXP, SEXP, SEXP, SEXP);
+SEXP R_deseir_finalize(void);
+void R_deseir_dot(int *, double *, double *, double *, double *, int *);
+void R_deseir_jac(int *, double *, double *, int *, int *, double *, int *,
+                  double *, int *);
 
 static const R_CallMethodDef CallMethods[] =
 {
-	{ "R_fastbeta",         (DL_FUNC) &R_fastbeta,         2 },
-	{ "R_ptpi",             (DL_FUNC) &R_ptpi,             8 },
+	{ "R_fastbeta",          (DL_FUNC) &R_fastbeta,          2 },
+	{ "R_ptpi",              (DL_FUNC) &R_ptpi,              8 },
 
-	{ "R_adsir_initialize", (DL_FUNC) &R_adsir_initialize, 5 },
-	{ "R_adsir_finalize",   (DL_FUNC) &R_adsir_finalize,   0 },
-	{ "R_adsir_dot",        (DL_FUNC) &R_adsir_dot,        2 },
-	{ "R_adsir_jac",        (DL_FUNC) &R_adsir_jac,        2 },
+	{ "R_adseir_initialize", (DL_FUNC) &R_adseir_initialize, 6 },
+	{ "R_adseir_finalize",   (DL_FUNC) &R_adseir_finalize,   0 },
+	{ "R_adseir_dot",        (DL_FUNC) &R_adseir_dot,        2 },
+	{ "R_adseir_jac",        (DL_FUNC) &R_adseir_jac,        2 },
 
-	{ "R_desir_initialize", (DL_FUNC) &R_desir_initialize, 5 },
-	{ "R_desir_finalize",   (DL_FUNC) &R_desir_finalize,   0 },
+	{ "R_deseir_initialize", (DL_FUNC) &R_deseir_initialize, 6 },
+	{ "R_deseir_finalize",   (DL_FUNC) &R_deseir_finalize,   0 },
 
 	{ NULL, NULL, 0 }
 };
@@ -38,8 +38,8 @@ static R_NativePrimitiveArgType
 
 static const R_CMethodDef CMethods[] =
 {
-	{ "R_desir_dot",        (DL_FUNC) &R_desir_dot,        6, type0 },
-	{ "R_desir_jac",        (DL_FUNC) &R_desir_jac,        9, type1 },
+	{ "R_deseir_dot",        (DL_FUNC) &R_deseir_dot,        6, type0 },
+	{ "R_deseir_jac",        (DL_FUNC) &R_deseir_jac,        9, type1 },
 
 	{ NULL, NULL, 0, NULL }
 };
--- src/seir.c
+++ src/seir.c
@@ -1,10 +1,13 @@
 #include <math.h> /* exp */
 #include <string.h> /* memset */
 #include <Rinternals.h>
+#include <R_ext/RS.h>
 
-static SEXP F, DF, betaCall, betaArg, nuCall, nuArg, muCall, muArg, s;
-static double *pF, *pDF, *pbetaArg, betaVal, *pnuArg, nuVal, *pmuArg, muVal,
-	gammaVal, deltaVal, lastTimeDot, lastTimeJac, tmp, *pt, *px;
+static SEXP FF, DF, betaCall, betaArg, nuCall, nuArg, muCall, muArg, s;
+static double *pFF, *pDF, *pbetaArg, betaVal, *pnuArg, nuVal, *pmuArg, muVal,
+	sigmaVal, gammaVal, deltaVal, lastTimeDot, lastTimeJac, *pt, *px,
+	tmp, *work0, *work1, swork1, *work2, swork2;
+static int m, n, p, nrow, ncol, ok;
 
 #define INIT_CALL(_V_) \
 do { \
@@ -17,7 +20,7 @@ do { \
 	p ## _V_ ## Arg = REAL(_V_ ## Arg); \
 } while (0)
 
-#define TRY_EVAL_CALL(_V_) \
+#define EVAL_CALL(_V_) \
 do { \
 	s = eval(_V_ ## Call, R_GlobalEnv); \
 	if (TYPEOF(s) != REALSXP) \
@@ -29,42 +32,65 @@ do { \
 		error("'%s' returned a nonfinite or negative value", #_V_); \
 } while (0)
 
-#define MAYBE_EVAL_CALL(_T1_, _T0_) \
+#define SETUP(_MODE_, _T0_, _T1_, _X_) \
 do { \
-	if (_T1_ != _T0_) { \
+	if (_T0_ != _T1_) { \
 		*pbetaArg = *pnuArg = *pmuArg = _T1_; \
-		TRY_EVAL_CALL(beta); \
-		TRY_EVAL_CALL(nu); \
-		TRY_EVAL_CALL(mu); \
-		 _T0_ = _T1_; \
+		EVAL_CALL(beta); \
+		EVAL_CALL(nu); \
+		EVAL_CALL(mu); \
+		_T0_ = _T1_; \
+		R_ ## _MODE_ ## _setup(_X_); \
 	} \
 } while (0)
 
-SEXP R_adsir_initialize(SEXP beta, SEXP nu, SEXP mu, SEXP gamma, SEXP delta)
+SEXP R_adseir_initialize(SEXP beta, SEXP nu, SEXP mu, SEXP constants,
+                         SEXP sm, SEXP sn)
 {
-	F = allocVector(REALSXP, 7);
-	R_PreserveObject(F);
-	pF = REAL(F);
-	memset(pF, 0, 7 * sizeof(double));
+	m = INTEGER(sm)[0];
+	n = INTEGER(sn)[0];
+	p = m + n + 2;
 
-	DF = allocMatrix(REALSXP, 5, 7);
+	nrow = p + 2;
+	ncol = p + p + 1;
+
+	FF = allocVector(REALSXP, ncol);
+	R_PreserveObject(FF);
+	pFF = REAL(FF);
+	memset(pFF, 0, LENGTH(FF) * sizeof(double));
+
+	DF = allocMatrix(REALSXP, nrow, ncol);
 	R_PreserveObject(DF);
 	pDF = REAL(DF);
-	memset(pDF, 0, 35 * sizeof(double));
+	memset(pDF, 0, LENGTH(DF) * sizeof(double));
 
 	INIT_CALL(beta);
 	INIT_CALL(nu);
 	INIT_CALL(mu);
-	gammaVal = REAL(gamma)[0];
-	deltaVal = REAL(delta)[0];
+	sigmaVal = REAL(constants)[p + 0] * (double) m;
+	gammaVal = REAL(constants)[p + 1] * (double) n;
+	deltaVal = REAL(constants)[p + 2] * (double) 1;
+
+	pDF += nrow * (2 + p) + 1;
+	for (int i = 0; i < m; ++i)
+	{
+		*(pDF++) = sigmaVal;
+		pDF += nrow;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		*(pDF++) = gammaVal;
+		pDF += nrow;
+	}
+	*pDF = deltaVal;
 
 	lastTimeDot = lastTimeJac = -1.0;
 	return R_NilValue;
 }
 
-SEXP R_adsir_finalize(void)
+SEXP R_adseir_finalize(void)
 {
-	R_ReleaseObject(F);
+	R_ReleaseObject(FF);
 	R_ReleaseObject(DF);
 	R_ReleaseObject(betaCall);
 	R_ReleaseObject(betaArg);
@@ -75,49 +101,121 @@ SEXP R_adsir_finalize(void)
 	return R_NilValue;
 }
 
-SEXP R_adsir_dot(SEXP t, SEXP x)
+static
+void R_adseir_setup(double *px)
 {
-	pt = REAL(t);
-	px = REAL(x);
-	MAYBE_EVAL_CALL(*pt, lastTimeDot);
-	pF[0] = nuVal;
-	pF[1] = betaVal * px[0] * px[1];
-	pF[2] = (px[1] > 1.0) ? gammaVal * px[1] : 0.0;
-	pF[3] = muVal * px[0];
-	pF[4] = (px[1] > 1.0) ? muVal * px[1] : 0.0;
-	pF[5] = muVal * px[2];
-	pF[6] = deltaVal * px[2];
-	return F;
+	px++;
+
+	swork1 = swork2 = 0.0;
+	for (int i = 0; i < m; ++i)
+	{
+		swork1 += *(px++);
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		swork2 += *(px++);
+	}
+	ok = swork1 + swork2 > 1.0;
+
+	return;
 }
 
-SEXP R_adsir_jac(SEXP t, SEXP x)
+SEXP R_adseir_dot(SEXP t, SEXP x)
 {
 	pt = REAL(t);
 	px = REAL(x);
-	MAYBE_EVAL_CALL(*pt, lastTimeJac);
-	pDF[ 5] = betaVal * px[1];
-	pDF[ 6] = betaVal * px[0];
-	pDF[11] = gammaVal;
-	pDF[15] = muVal;
-	pDF[21] = muVal;
-	pDF[27] = muVal;
-	pDF[32] = deltaVal;
+	SETUP(adseir, lastTimeDot, *pt, px);
+
+	pFF = REAL(FF);
+	*(pFF++) = betaVal * px[0] * swork2;
+	*(pFF++) = nuVal;
+	if (ok)
+	{
+		for (int i = 0; i < p; ++i)
+		{
+			*(pFF++) = muVal * px[i];
+		}
+	}
+	else
+	{
+		memset(pFF, 0, p * sizeof(double));
+		pFF[    0] = muVal * px[    0];
+		pFF[p - 1] = muVal * px[p - 1];
+		pFF += p;
+	}
+	for (int i = 0; i < m; ++i)
+	{
+		*(pFF++) = sigmaVal * px[1 + i];
+	}
+	if (ok)
+	{
+		for (int i = 0; i < n; ++i)
+		{
+			*(pFF++) = gammaVal * px[1 + m + i];
+		}
+	}
+	else
+	{
+		memset(pFF, 0, n * sizeof(double));
+		pFF += n;
+	}
+	*(pFF++) = deltaVal * px[1 + m + n];
+
+	return FF;
+}
+
+SEXP R_adseir_jac(SEXP t, SEXP x)
+{
+	pt = REAL(t);
+	px = REAL(x);
+	SETUP(adseir, lastTimeDot, *pt, px);
+
+	tmp = betaVal * px[0];
+
+	pDF = REAL(DF);
+	*(pDF++) = betaVal * swork2;
+	pDF += m;
+	for (int i = 0; i < n; ++i)
+	{
+		*(pDF++) = tmp;
+	}
+
+	pDF += 3 + nrow;
+	for (int i = 0; i < p; ++i)
+	{
+		*(pDF++) = muVal;
+		pDF += nrow;
+	}
+
 	return DF;
 }
 
-SEXP R_desir_initialize(SEXP beta, SEXP nu, SEXP mu, SEXP gamma, SEXP delta)
+SEXP R_deseir_initialize(SEXP beta, SEXP nu, SEXP mu, SEXP constants,
+                         SEXP sm, SEXP sn)
 {
+	m = INTEGER(sm)[0];
+	n = INTEGER(sn)[0];
+	p = m + n + 2;
+
+	nrow = p + 2;
+	ncol = p + 2;
+
 	INIT_CALL(beta);
 	INIT_CALL(nu);
 	INIT_CALL(mu);
-	gammaVal = REAL(gamma)[0];
-	deltaVal = REAL(delta)[0];
+	sigmaVal = REAL(constants)[p + 0] * (double) m;
+	gammaVal = REAL(constants)[p + 1] * (double) n;
+	deltaVal = REAL(constants)[p + 2] * (double) 1;
+
+	work0 = R_Calloc(m + 3 * n, double);
+	work1 = work0 + m + n;
+	work2 = work1 +     n;
 
 	lastTimeDot = lastTimeJac = -1.0;
 	return R_NilValue;
 }
 
-SEXP R_desir_finalize(void)
+SEXP R_deseir_finalize(void)
 {
 	R_ReleaseObject(betaCall);
 	R_ReleaseObject(betaArg);
@@ -125,40 +223,92 @@ SEXP R_desir_finalize(void)
 	R_ReleaseObject(nuArg);
 	R_ReleaseObject(muCall);
 	R_ReleaseObject(muArg);
+	R_Free(work0);
 	return R_NilValue;
 }
 
-/* vignette("compiledCode", package = "deSolve") */
-void R_desir_dot(int *neq, double *t, double *y, double *ydot,
-                 double *yout, int *ip)
+static
+void R_deseir_setup(double *px)
 {
-	MAYBE_EVAL_CALL(*t, lastTimeDot);
-	tmp = y[1];
-	y[1] = exp(tmp);
-	ydot[0] = nuVal - betaVal * y[0] * y[1] + deltaVal * y[2] - muVal * y[0];
-	ydot[1] = betaVal * y[0] - gammaVal - muVal;
-	ydot[2] = gammaVal * y[1] - deltaVal * y[2] - muVal * y[2];
-	ydot[3] = nuVal;
-	ydot[4] = betaVal * y[0] * y[1];
-	y[1] = tmp;
+	px++;
+	double *py = px + 1;
+
+	tmp = *px;
+
+	swork1 = swork2 = 0.0;
+	for (int i = 0; i < m; ++i)
+	{
+		work0[    i] = exp(*(px++) - *(py++));
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		swork1 += (work0[i] = exp(*px      ));
+		swork2 += (work1[i] = exp(*px - tmp));
+		work0[m + i] = exp(*(px++) - *(py++));
+	}
+
 	return;
 }
 
 /* vignette("compiledCode", package = "deSolve") */
-void R_desir_jac(int *neq, double *t, double *y, int *ml,
-                 int *mu, double *pd, int *nrowpd, double *yout, int *ip)
+void R_deseir_dot(int *neq, double *t, double *y, double *ydot,
+                  double *yout, int *ip)
 {
-	MAYBE_EVAL_CALL(*t, lastTimeJac);
-	tmp = y[1];
-	y[1] = exp(tmp);
-	pd[ 0] = -betaVal * y[1] - muVal;
-	pd[ 1] = betaVal;
-	pd[ 4] = betaVal * y[1];
-	pd[ 5] = -betaVal * y[0] * y[1];
-	pd[ 7] = gammaVal * y[1];
-	pd[ 9] = betaVal * y[0] * y[1];
-	pd[10] = deltaVal;
-	pd[12] = -deltaVal - muVal;
-	y[1] = tmp;
+	SETUP(deseir, lastTimeDot, *t, y);
+
+	*(ydot++) = nuVal - betaVal * y[0] * swork1 + deltaVal * y[p - 1] - muVal * y[0];
+	if (m == 0)
+	*(ydot++) = betaVal * y[0] * swork2 - gammaVal - muVal;
+	else
+	{
+	*(ydot++) = betaVal * y[0] * swork2 - sigmaVal - muVal;
+	for (int i = 0; i < m - 1; ++i)
+	{
+	*(ydot++) = sigmaVal * work0[i    ] - sigmaVal - muVal;
+	}
+	*(ydot++) = sigmaVal * work0[m - 1] - gammaVal - muVal;
+	}
+	for (int i = 0; i < n - 1; ++i)
+	{
+	*(ydot++) = gammaVal * work0[m + i] - gammaVal - muVal;
+	}
+	*(ydot++) = gammaVal * work0[m + n] - deltaVal - muVal;
+	*(ydot++) = betaVal * y[0] * swork1;
+	*(ydot++) = nuVal;
+
+	return;
+}
+
+/* vignette("compiledCode", package = "deSolve") */
+void R_deseir_jac(int *neq, double *t, double *y, int *ml,
+                  int *mu, double *pd, int *nrowpd, double *yout, int *ip)
+{
+	SETUP(deseir, lastTimeDot, *t, y);
+
+	pd[0] = -(pd[p] = betaVal * swork1) - muVal;
+	pd[1] =           betaVal * swork2         ;
+	pd += nrow + 2;
+	for (int i = 0; i < m; ++i)
+	{
+		*(pd + nrow) = -(*pd = sigmaVal * work0[    i]);
+		*pd += nrow + 1;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		*(pd + nrow) = -(*pd = gammaVal * work0[m + i]);
+		*pd += nrow + 1;
+	}
+	pd -= p;
+	pd[0] = deltaVal * exp(y[p - 1]);
+	pd -= n * nrow;
+	for (int i = 0; i < n; ++i)
+	{
+		pd[0] = -(pd[p] = betaVal * y[0] * work1[i]);
+		pd[1] =           betaVal * y[0] * work2[i] ;
+		pd += nrow;
+	}
+	pd -= (m + n) * nrow;
+	pd[1] = (m == 0) ? 0.0 : -betaVal * swork2;
+
 	return;
 }
