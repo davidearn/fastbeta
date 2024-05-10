2f3b3d8fc6bb581604e070e9c73724be18f3d87f
Mikael Jagan <jaganmn@mcmaster.ca>
2024-05-10 17:52:43 -0400

    fix up 'data' scripts

--- data/seir.E01.R
+++ data/seir.E01.R
@@ -1,31 +1,37 @@
-## NB: sir.e02 uses the same parameters but sets stochastic=TRUE
+## NB: seir.E02 differs only in setting of 'stochastic'
 
-sir.e01 <- local({
-beta <- function (t, a = 1e-01, b = 1e-05)
-	b * (1 + a * cospi(t / 26))
+seir.E01 <- local({
+stochastic <- FALSE
+
+beta <- function (t, a = 1e-01, b = 1e-05) b * (1 + a * sinpi(t / 26))
 nu <- function (t) 1e+03
 mu <- function (t) 1e-03
 environment(beta) <- environment(nu) <- environment(mu) <- .GlobalEnv
 
-S0 <- 5e+04
-I0 <- 1e+03
-R0 <- 1e+06 - S0 - I0
+sigma <- 0.5
 gamma <- 0.5
 delta <- 0
-constants <- c(S0 = S0, I0 = I0, R0 = R0, gamma = gamma, delta = delta)
 
-n <- 52L * 55L
+m <- 1L
+n <- 1L
+init <- fastbeta:::seir.ee(beta(0), nu(0), mu(0), sigma, gamma, delta, m, n)
+
+length.out <- 52L * 55L
 prob <- 0.1
 delay <- diff(stats::pgamma(0L:8L, 2.5))
 
-
-X <- fastbeta::sir(n, beta, nu, mu, constants,
-                   prob = prob, delay = delay,
-                   stochastic = FALSE)
-X <- stats::window(X, start = n - 52L * 5L)
+X <- fastbeta::seir(length.out,
+                    beta, nu, mu,
+                    sigma, gamma, delta,
+                    init, m, n,
+                    stochastic = stochastic,
+                    prob = prob, delay = delay)
+X <- stats::window(X, start = length.out - 1L - 52L * 5L)
 stats::tsp(X)[1L:2L] <- stats::tsp(X)[1L:2L] - stats::tsp(X)[1L]
 
 structure(X,
-          beta = beta, nu = nu, mu = mu, gamma = gamma, delta = delta,
+          beta = beta, nu = nu, mu = mu,
+          sigma = sigma, gamma = gamma, delta = delta,
+          stochastic = stochastic,
           prob = prob, delay = delay)
 })
