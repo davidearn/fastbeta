494be1c5550d85abf56f69fe2ac4249164b75227
Mikael Jagan <jaganmn@mcmaster.ca>
2025-04-29 13:34:55 -0400

    file name convention: seir_<module>.<extension>

--- ms/sandbox/seir_stan.R
+++ ms/sandbox/seir_stan.R
@@ -1,3 +1,3 @@
 source("seir.R"); stopifnot(is.list(data))
 options(mc.cores = parallel::detectCores())
-fit <- rstan::stan("seir.stan.stan", data = data, init = "0")
+fit <- rstan::stan("seir_stan.stan", data = data, init = "0")
